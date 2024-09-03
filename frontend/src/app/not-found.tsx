691daa45e2fd7319772c498d9369353968c89d1e
shivam6862 <shivam6862mau@gmail.com>
2024-09-03 23:18:13 +0530

    error, loading and not-found page

---
+++ frontend/src/app/not-found.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          color: "white",
+          paddingTop: "3rem",
+        }}
+      >
+        <h1>404 - Page Not Found</h1>
+        <p>
+          The page you are looking for might have been removed had its name
+          changed or is temporarily unavailable.
+        </p>
+        <Link href="/">
+          <div
+            style={{
+              backgroundColor: "var(--primary-color)",
+              color: "white",
+              padding: "0.5rem 1rem",
+              borderRadius: "5px",
+              cursor: "pointer",
+              marginTop: "1rem",
+            }}
+          >
+            Go to Home Page
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+}
