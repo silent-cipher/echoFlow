b97a969fd113da2968ddc8ac7cf1531627ba6c13
shivam6862 <shivam6862mau@gmail.com>
2024-09-03 18:18:17 +0530

    updated error and loading components

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -15,7 +15,7 @@ export default function NotFound() {
         }}
       >
         <h1>404 - Page Not Found</h1>
-        <p>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p>
+        <p>The page you are looking for might have been removed had its name changed or is temporarily unavailable. </p>
         <Link href="/">
           <div
             style={{
