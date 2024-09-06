06d9d028209b72839db6addccb225cfb935ab1aa
shivam6862 <shivam6862mau@gmail.com>
2024-09-07 01:57:04 +0530

    added more urls in manifests

--- twitter-get-tweet-tool/popup.js
+++ twitter-get-tweet-tool/popup.js
@@ -130,7 +130,8 @@ document.addEventListener("DOMContentLoaded", async () => {
   const activeTab = await getActiveTabURL();
   if (
     activeTab.url.includes("twitter.com") ||
-    activeTab.url.includes("http://localhost:3000/")
+    activeTab.url.includes("http://localhost:3000/") ||
+    activeTab.url.includes("https://x.com/")
   ) {
     chrome.tabs.sendMessage(
       activeTab.id,
